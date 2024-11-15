8cb0fe81ccbc62044a29a45ab79686d572d78107
pc <fahlaouimohammed@gmail.com>
2024-11-15 02:11:16 +0100

    modif

--- project/src/main/java/ma/ensa/project/ApplicationGestionFacturation.java
+++ project/src/main/java/ma/ensa/project/ApplicationGestionFacturation.java
@@ -1,14 +1,12 @@
 package ma.ensa.project;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 import ma.ensa.project.controller.Login;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class ApplicationGestionFacturation extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         Login login = new Login();
--- project/src/main/java/ma/ensa/project/controller/Login.java
+++ project/src/main/java/ma/ensa/project/controller/Login.java
@@ -3,14 +3,14 @@ package ma.ensa.project.controller;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import ma.ensa.project.HelloApplication;
+import ma.ensa.project.ApplicationGestionFacturation;
 
 import java.io.IOException;
 
 public class Login {
 
     public void start() throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(ApplicationGestionFacturation.class.getResource("login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         Stage stage = new Stage();
         stage.setTitle("Login");
