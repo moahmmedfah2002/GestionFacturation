7a8f2a834929c5842a4cfb32b500ae091234e984
pc <fahlaouimohammed@gmail.com>
2024-12-08 14:50:05 +0100

    test

---
+++ project/src/main/java/ma/ensa/project/test/Test1.java
@@ -0,0 +1,13 @@
+package ma.ensa.project.test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.Assertion;
+
+public class Test1 {
+    @Test
+    public void t(){
+        Assert.assertEquals(1,1);
+
+    }
+}
