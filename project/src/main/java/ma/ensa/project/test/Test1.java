a6dfb1bb53f231c09f9ba58375aa14f3db152ec6
pc <fahlaouimohammed@gmail.com>
2024-12-09 20:11:48 +0100

    phase1

--- project/src/main/java/ma/ensa/project/test/Test1.java
+++ project/src/main/java/ma/ensa/project/test/Test1.java
@@ -1,13 +1,20 @@
 package ma.ensa.project.test;
 
+import ma.ensa.project.service.MailService;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.testng.asserts.Assertion;
 
 public class Test1 {
+
     @Test
-    public void t(){
-        Assert.assertEquals(1,1);
+    public void mail(){
+        //kawtar.zouiher.02@gmail.com
+        //bilallaariny01@gmail.com
+        MailService mailService = new MailService();
+        boolean m=mailService.send("fahlaouimohammed@gmail.com","test","chouf lta7t","C:\\Users\\pc\\Documents\\java2\\GestionFacturation\\project\\src\\main\\resources\\f.pdf");
+        Assert.assertTrue(m);
+
 
     }
 }
--- project/src/main/java/module-info.java
+++ project/src/main/java/module-info.java
@@ -8,11 +8,17 @@ module ma.ensa.project {
     requires static lombok;
     requires java.desktop;
     requires org.testng;
-    requires org.junit.platform.commons;
+//    requires org.junit.platform.commons;
+    requires java.mail;
+    requires activation;
+    requires MaterialFX;
+
 
     opens ma.ensa.project to javafx.fxml;
     exports ma.ensa.project;
+    exports ma.ensa.project.entity;
     exports ma.ensa.project.test;
     exports ma.ensa.project.controller;
+
     opens ma.ensa.project.controller to javafx.fxml;
 }
