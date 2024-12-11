653a5cf61f56f3bd88fba0df6ed58fef4a47f938
pc <fahlaouimohammed@gmail.com>
2024-12-11 21:48:23 +0100

    login session

--- project/src/main/java/ma/ensa/project/test/Test1.java
+++ project/src/main/java/ma/ensa/project/test/Test1.java
@@ -10,26 +10,29 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.testng.asserts.Assertion;
 
+import java.io.IOException;
 import java.sql.SQLException;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Test1 {
 
+//    @Test
+//    public void mail(){
+//        //kawtar.zouiher.02@gmail.com
+//        //bilallaariny01@gmail.com
+//        MailService mailService = new MailService();
+//        boolean m=mailService.send("fahlaouimohammed@gmail.com","test","chouf lta7t","C:\\Users\\pc\\Documents\\java2\\GestionFacturation\\project\\src\\main\\resources\\f.pdf");
+//        Assert.assertTrue(m);
+//
+//
+//    }
     @Test
-    public void mail(){
-        //kawtar.zouiher.02@gmail.com
-        //bilallaariny01@gmail.com
-        MailService mailService = new MailService();
-        boolean m=mailService.send("fahlaouimohammed@gmail.com","test","chouf lta7t","C:\\Users\\pc\\Documents\\java2\\GestionFacturation\\project\\src\\main\\resources\\f.pdf");
-        Assert.assertTrue(m);
-
-
-    }
-    @Test
-    public void add() throws SQLException, ClassNotFoundException {
+    public void add() throws SQLException, ClassNotFoundException, IOException {
         UserService userService = new UserService();
         User user = new User();
+        user.setId(8);
         user.setNomUtilisateur("moh");
         user.setMotDePasse("hi");
         user.setRole(Role.ADMIN.toString());
@@ -45,5 +48,6 @@ public class Test1 {
 
 
     }
+
 
 }
